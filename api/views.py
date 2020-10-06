0f93dce2bd31d4ad7d8360e2eb5c575b59636ed7
lee <vineeth.1301t@gmail.com>
2020-10-06 14:02:55 +0530

    Using BaseView

--- api/views.py
+++ api/views.py
@@ -10,6 +10,7 @@ from rest_framework import status
 from django.http import HttpResponse
 from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError
+from .baseview import BaseDetails
 import uuid
 
 
@@ -24,84 +25,16 @@ class UserList(generics.ListCreateAPIView):
             return Response({"status":"true","message":"data Posted succesfully.","data":{"uuid": serializer.data['uuid']}}, status=status.HTTP_201_CREATED)
         return Response({'message': 'user with this email already exist',}, status=status.HTTP_400_BAD_REQUEST)
 
-class UserDetails(APIView):
+class UserDetails(BaseDetails):
     model_class = User
     serializer_class = UserSerializer
     head = "user"
 
-    def get_object(self, pk):
-        try:
-            return self.model_class.objects.get(pk=pk)    
-        except self.model_class.DoesNotExist:
-            raise ValidationError({
-                'status': False,
-                'message': f"failed to find {self.head}",
-                "data": {}
-            })
 
-    def get(self, request, pk, format=None):
-        obj = self.get_object(pk)
-        serializer = self.serializer_class(obj)
-        return Response(
-            data={
-                "status": True,
-                "message": f"{self.head} data reterived sucessfully",
-                "data": serializer.data
-            })
-
-    def put(self, request, pk, format=None):
-        obj = self.get_object(pk)
-        serializer = self.serializer_class(obj, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(
-                data={
-                    "status": True,
-                    "message": f"{self.head} updated sucessfully",
-                    "data": serializer.data
-                })
-        return Response(data={
-            "status": False,
-            "message": f"{self.head} update failed",
-            "data": serializer.errors
-        },
-                        status=status.HTTP_400_BAD_REQUEST)
-
-    def patch(self, request, pk, format=None):
-        obj = self.get_object(pk)
-        serializer = self.serializer_class(obj,
-                                           data=request.data,
-                                           partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(
-                data={
-                    "status": True,
-                    "message": f"{self.head} updated sucessfully",
-                    "data": serializer.data
-                })
-        return Response(data={
-            "status": False,
-            "message": f"{self.head} update failed",
-            "data": serializer.errors
-        },
-                        status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        obj = self.get_object(pk)
-        obj.delete()
-        return Response(data={
-            "status": True,
-            "message": f"{self.head} deleted sucessfully",
-            "data": {}
-        },
-                        status=status.HTTP_200_OK)
-
-
-class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Profile.objects.all()
+class ProfileDetail(BaseDetails):
+    model_class = Profile
     serializer_class = ProfileSerializer
-
+    head = "profile"
 
 class ProfileList(generics.ListCreateAPIView):
     queryset = Profile.objects.all()
@@ -112,38 +45,24 @@ class ProfileList(generics.ListCreateAPIView):
         serializer = ProfileSerializer(profile, many = True)
         return Response({"status": "true", "message": "data Retrieve successfully.", "data": serializer.data})
 
-'''
-class ProfileAPI(APIView):
-    def get(self, request, *args, **kwargs):
-        user = get_object_or_404(User, pk=kwargs['user_id'])
-        profile_serializer = ProfileSerializer(user.profile)
-        return Response(profile_serializer.data)
-
-'''
-
-
-class EducationDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Education.objects.all()
+class EducationDetail(BaseDetails):
+    model_class = Education
     serializer_class = EducationSerializer
-
+    head = "education"
 
 class EducationList(generics.ListCreateAPIView):
     queryset = Education.objects.all()
     serializer_class = EducationSerializer
-
     
     def get(self, request, **kwargs):
         education = Education.objects.all()
         serializer = EducationSerializer(education, many = True)
         return Response({"status": "true", "message": "data Retrieve successfully.", "data": serializer.data})
 
-
-
-class ExperienceDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Experience.objects.all()
+class ExperienceDetail(BaseDetails):
+    model_class = Experience
     serializer_class = ExperienceSerializer
-
-
+    head = "experience"
 
 class ExperienceList(generics.ListCreateAPIView):
     queryset = Experience.objects.all()
@@ -162,3 +81,14 @@ class Feedpage(generics.RetrieveUpdateDestroyAPIView):
         fed = Feed.objects.all()
         serializer = FeedSerializer(fed, many=True)
         return Response({"status": "true", "message": "data Retrieve successfully.", "data": serializer.data})
+
+
+
+'''
+class ProfileAPI(APIView):
+    def get(self, request, *args, **kwargs):
+        user = get_object_or_404(User, pk=kwargs['user_id'])
+        profile_serializer = ProfileSerializer(user.profile)
+        return Response(profile_serializer.data)
+
+'''
